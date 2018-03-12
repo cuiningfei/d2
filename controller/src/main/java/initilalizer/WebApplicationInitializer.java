e1c699d860c93e682a67aa6b59e501088847b54f
cuiningfei <3544948673@qq.com>
2018-03-12 10:58:40 +0800

    Initial commit

---
+++ controller/src/main/java/initilalizer/WebApplicationInitializer.java
@@ -0,0 +1,25 @@
+package initilalizer;
+
+import config.RootConfig;
+import config.WebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+public class WebApplicationInitializer
+        extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{RootConfig.class,};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ service/src/main/java/service/UserService.java
@@ -0,0 +1,11 @@
+package service;
+
+import domain.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> list();
+    User add(User user);
+}
